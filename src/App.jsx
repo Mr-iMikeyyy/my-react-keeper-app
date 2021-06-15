9c6c8a5c88772e70c36dcbe484296c19fd4688af
Mr-iMikeyyy <56235937+Mr-iMikeyyy@users.noreply.github.com>
2021-06-15 09:42:25 -0400

    created a dynamically updated note function, changed css

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,19 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Note from "./components/Note";
+import notes from "./notes";
+
+function createNote (noteEntry) {
+  return(
+  <Note key={noteEntry.id} title={noteEntry.title} content={noteEntry.content} />
+  )
+}
 
 function App() {
   return (
       <div>
         <Header />
-        <Note />
+        {notes.map(createNote)}
         <Footer />
       </div>
   );
